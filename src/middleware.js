e419669a2ff3d3d09f40bfe1f994b37591ecf2bd
mehedi.mosh <mehedi.mosh@gmail.com>
2024-06-12 16:53:11 +0600

    auth fixed

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export const middleware = async (request) => {
-  const token = cookies(request).get("next-auth.session-token");
+  const token = cookies(request).get("__Secure-next-auth.session-token");
   const pathname = request.nextUrl.pathname
   if(pathname.includes('api')) {
       return NextResponse.next();
